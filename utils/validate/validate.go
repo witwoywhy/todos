43a3e95d4a78f0fb3b15f232ba9e085d85ee5221
witwoywhy <but.nuttawit.dev@gmail.com>
2024-01-24 20:28:39 +0700

    feat: add validate format datetime

--- utils/validate/validate.go
+++ utils/validate/validate.go
@@ -2,6 +2,7 @@ package validate
 
 import (
 	"fmt"
+	"time"
 	"todos/utils/constants"
 
 	"github.com/go-playground/validator/v10"
@@ -15,6 +16,10 @@ func InitValidate() {
 	if err := Validator.RegisterValidation("status", validateStatus); err != nil {
 		panic(fmt.Errorf("failed to register custom validate status: %v", err))
 	}
+
+	if err := Validator.RegisterValidation("RFC3339", rfc3339); err != nil {
+		panic(fmt.Errorf("failed to register custom validate RFC3339: %v", err))
+	}
 }
 
 func validateStatus(fl validator.FieldLevel) bool {
@@ -24,4 +29,9 @@ func validateStatus(fl validator.FieldLevel) bool {
 	}
 
 	return true
+}
+
+func rfc3339(fl validator.FieldLevel) bool {
+	_, err := time.Parse(time.RFC3339, fl.Field().String())
+	return err == nil
 }
