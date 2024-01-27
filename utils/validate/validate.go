0cd03221b7c576e47ab82312825df4436a88172d
witwoywhy <but.nuttawit.dev@gmail.com>
2024-01-27 19:09:49 +0700

    feat: validate sort field & order

--- utils/validate/validate.go
+++ utils/validate/validate.go
@@ -20,6 +20,14 @@ func InitValidate() {
 	if err := Validator.RegisterValidation("RFC3339", rfc3339); err != nil {
 		panic(fmt.Errorf("failed to register custom validate RFC3339: %v", err))
 	}
+
+	if err := Validator.RegisterValidation("sortField", sortField); err != nil {
+		panic(fmt.Errorf("failed to register custom validate sortField: %v", err))
+	}
+
+	if err := Validator.RegisterValidation("sortOrder", sortOrder); err != nil {
+		panic(fmt.Errorf("failed to register custom validate sortOrder: %v", err))
+	}
 }
 
 func validateStatus(fl validator.FieldLevel) bool {
@@ -34,4 +42,22 @@ func validateStatus(fl validator.FieldLevel) bool {
 func rfc3339(fl validator.FieldLevel) bool {
 	_, err := time.Parse(time.RFC3339, fl.Field().String())
 	return err == nil
+}
+
+func sortField(fl validator.FieldLevel) bool {
+	field := fl.Field().String()
+	if field != constants.SortTitle && field != constants.SortStatus && field != constants.SortDate {
+		return false
+	}
+
+	return true
+}
+
+func sortOrder(fl validator.FieldLevel) bool {
+	field := fl.Field().String()
+	if field != constants.SortDesc && field != constants.SortAsc {
+		return false
+	}
+
+	return true
 }
--- utils/validate/validate_test.go
+++ utils/validate/validate_test.go
@@ -45,3 +45,124 @@ func TestValidateStatus(t *testing.T) {
 		})
 	}
 }
+
+func TestRFC3339(t *testing.T) {
+	validate.InitValidate()
+
+	type request struct {
+		Date string `validate:"RFC3339"`
+	}
+
+	type testCase struct {
+		name     string
+		given    request
+		isPassed bool
+	}
+
+	testCases := []testCase{
+		{
+			name: "passed",
+			given: request{
+				Date: "2024-01-08T00:00:00+07:00",
+			},
+			isPassed: true,
+		},
+		{
+			name: "failed",
+			given: request{
+				Date: "2024-01-08T00:00:00Z07:00",
+			},
+			isPassed: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := validate.Validator.Struct(tc.given)
+			assert.Equal(t, tc.isPassed, err == nil)
+		})
+	}
+}
+
+func TestSortField(t *testing.T) {
+	validate.InitValidate()
+
+	type request struct {
+		Field string `validate:"sortField"`
+	}
+
+	type testCase struct {
+		name     string
+		given    request
+		isPassed bool
+	}
+
+	testCases := []testCase{
+		{
+			name: "passed",
+			given: request{
+				Field: "status",
+			},
+			isPassed: true,
+		},
+		{
+			name: "failed",
+			given: request{
+				Field: "image",
+			},
+			isPassed: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := validate.Validator.Struct(tc.given)
+			assert.Equal(t, tc.isPassed, err == nil)
+		})
+	}
+}
+
+func TestSortOrder(t *testing.T) {
+	validate.InitValidate()
+
+	type request struct {
+		Order string `validate:"sortOrder"`
+	}
+
+	type testCase struct {
+		name     string
+		given    request
+		isPassed bool
+	}
+
+	testCases := []testCase{
+		{
+			name: "passed",
+			given: request{
+				Order: "desc",
+			},
+			isPassed: true,
+		},
+		{
+			name: "passed",
+			given: request{
+				Order: "asc",
+			},
+			isPassed: true,
+		},
+		{
+			name: "failed",
+			given: request{
+				Order: "descasc",
+			},
+			isPassed: false,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := validate.Validator.Struct(tc.given)
+			assert.Equal(t, tc.isPassed, err == nil)
+		})
+	}
+}
